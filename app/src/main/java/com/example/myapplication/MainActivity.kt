0171c6a1597d729d92a677989bf45c0802886d88
UpscalePilot <benjaminamorley@gmail.com>
2024-02-12 17:27:28 -0700

    Scheduling activity page added, linked to with button from splash screen
    Signed-off-by: UpscalePilot <benjaminamorley@gmail.com>

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.scheduling)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, Scheduling::class.java)
+            startActivity(intent)
+        }
+
     }
 }
