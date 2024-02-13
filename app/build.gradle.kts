360aa17f78f5dcf614c474be692bb500d9e5bde0
Jamie T. McDonald <mcdonaldj227@mymacewan.ca>
2024-02-13 10:20:35 -0700

    added employee screens first time

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.tasks.SourceSet
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    //id("com.google.relay")
+    id("com.google.relay")
 }
 
 android {
@@ -43,6 +43,12 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.8"
+    }
 }
 
 dependencies {
@@ -51,6 +57,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.compose.foundation:foundation-layout-android:1.6.1")
+    implementation("androidx.compose.ui:ui-tooling-preview-android:1.6.1")
+    implementation("androidx.compose.material3:material3-android:1.2.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
