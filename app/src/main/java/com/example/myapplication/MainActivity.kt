e10a591cd4c436bfb230472f0dba4e2f99ae7177
Jamie T. McDonald <mcdonaldj227@mymacewan.ca>
2024-02-14 18:46:20 -0700

    Added new UI element through Relay as demo for team.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val buttonClick = findViewById<Button>(R.id.scheduling)
-        buttonClick.setOnClickListener {
+        val buttonClick1 = findViewById<Button>(R.id.scheduling)
+        buttonClick1.setOnClickListener {
             val intent = Intent(this, Scheduling::class.java)
             startActivity(intent)
         }
-
+        val buttonClickE = findViewById<Button>(R.id.employees)
+        buttonClickE.setOnClickListener {
+            val intent = Intent(this, Employees::class.java)
+            startActivity(intent)
+        }
     }
 }
