eb20ed3549e4a25fed48b8cd1b5614c79f5ffa0c
Jamie T. McDonald <mcdonaldj227@mymacewan.ca>
2024-02-25 03:59:01 -0700

    Release Build "LuLoo"
    v1.01-alpha
    
    - Corrected Transitions
    - Deployed correct navigation structure (was incorrect before - added homepage)
    - Corrected several bugs

---
+++ CMPT395LuLoo/app/src/main/java/com/example/cmpt395luloo/screens/HomeScreen.kt
@@ -0,0 +1,22 @@
+package com.example.cmpt395luloo.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+// possibly need navController (navController: NavController) later depending
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Welcome to the Home Screen!")
+        Text(text = "Maybe put relevant stuff here?")
+        }
+    }
