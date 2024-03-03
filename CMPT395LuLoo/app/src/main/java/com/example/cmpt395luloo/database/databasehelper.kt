8cdaac06bc63511f70449b436a136f9040489d70
Jamie T. McDonald <mcdonaldj227@mymacewan.ca>
2024-03-03 02:52:56 -0700

    Release Build "LuLoo"
    v1.03-alpha
    
    - added db structure
    - added 'add employee' button and page
    
    !warning! no delete feature yet when data added
    to basic db
    
    *multiple pushes, learning how to commit/push from android studio directly - Jamie*

---
+++ CMPT395LuLoo/app/src/main/java/com/example/cmpt395luloo/database/databasehelper.kt
@@ -0,0 +1,73 @@
+/**
+ *  database helper v1.0
+ *
+ *  some ref's used in the creation of our database:
+ *  - https://developer.android.com/training/data-storage/sqlite
+ *  - https://abhiandroid.com/database/sqlite
+ *  - https://www.freecodecamp.org/news/how-to-use-sqlite-database-with-android-studio/
+ *  - https://www.androidauthority.com/sqlite-primer-for-android-811987/
+ */
+
+package com.example.cmpt395luloo.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.example.cmpt395luloo.database.employees.Employee
+
+class DatabaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+
+    companion object {
+        private const val DATABASE_NAME = "EmployeeDatabase"
+        private const val DATABASE_VERSION = 1
+    }
+    // new table
+    override fun onCreate(db: SQLiteDatabase) {
+        val CREATE_EMPLOYEE_TABLE = ("CREATE TABLE employees ( "
+                + "id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "name TEXT, "
+                + "position TEXT )")
+        db.execSQL(CREATE_EMPLOYEE_TABLE)
+    }
+    // upgrade table
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        db.execSQL("DROP TABLE IF EXISTS employees")
+        onCreate(db)
+    }
+    // add employee
+    fun addEmployee(name: String, position: String): Boolean {
+        val db = this.writableDatabase
+        val contentValues = ContentValues()
+        contentValues.put("name", name)
+        contentValues.put("position", position)
+        val result = db.insert("employees", null, contentValues)
+        db.close()
+        return result != -1L
+    }
+    // get all employees for list
+    fun getAllEmployees(): List<Employee> {
+        val employees = ArrayList<Employee>()
+        val db = this.readableDatabase
+        val cursor = db.rawQuery("SELECT * FROM employees", null)
+        if (cursor.moveToFirst()) {
+            do {
+                val id = cursor.getInt(0)
+                val name = cursor.getString(1)
+                val position = cursor.getString(2)
+                employees.add(Employee(id, name, position))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        db.close()
+        return employees
+    }
+    // delete employee
+    fun deleteEmployee(id: Int): Boolean {
+        val db = this.writableDatabase
+        val result = db.delete("employees", "id = ?", arrayOf(id.toString())).toLong()
+        db.close()
+        return result != -1L
+    }
+}
